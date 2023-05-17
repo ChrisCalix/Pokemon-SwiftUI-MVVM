1cef868c5353550de5288568232416583fb869d0
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-16 23:44:01 -0400

    Initial Commit

---
+++ Pokemon SwiftUI MVVM/Pokemon_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_SwiftUI_MVVMApp.swift
+//  Pokemon SwiftUI MVVM
+//
+//  Created by Sonic on 16/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
